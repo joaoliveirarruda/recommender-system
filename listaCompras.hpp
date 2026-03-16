6f3a934adc690d862b7056d7335b19c868527806
joaoliveirarruda <joaoliveirarruda@gmail.com>
2026-03-15 23:12:11 -0300

    adiciona arquivo de cabeçalho, o modulo listaCompras em uma função mais geral e melhora o nome das variaveis

---
+++ listaCompras.hpp
@@ -0,0 +1,18 @@
+#ifndef LISTACOMPRAS_HPP
+#define LISTACOMPRAS_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+
+struct ListaCompras {
+    std::vector<std::string> clientesCodigoBase;
+    std::map<std::string, int> clienteIndiceInterno;
+    std::vector<std::string> produtosNomeDescritivo;
+    std::map<std::string, int> produtoIndiceInterno;
+    std::vector<std::vector<int>> historicoComprasPorCliente;
+};
+
+ListaCompras carregarCompras(const std::string& caminhoArquivo);
+
+#endif // LISTACOMPRAS_HPP
